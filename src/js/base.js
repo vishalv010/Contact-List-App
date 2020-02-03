12183eb4130ba0fbf9b8efdbe61406a54679fa78
vishalv010 <58140240+vishalv010@users.noreply.github.com>
2020-02-03 15:16:06 +0530

    Delete base.js

--- src/js/base.js
+++
@@ -1,7 +0,0 @@
-export const elements={
-    addButton:document.querySelector(".add-button"),
-    contactForm:document.querySelector(".contact-form"),
-    contactList:document.querySelector('.contact-list'),
-    contactTable:document.querySelector('.contact-table'),
-    page:document.querySelector('.pages')
-}
